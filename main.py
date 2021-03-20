6f10bac6a24e6393827a712b003b029ac07cc1fa
jayers <jayer007@odu.edu>
2021-03-20 00:34:12 -0400

    Stepwise random notes

--- main.py
+++ main.py
@@ -11,22 +11,53 @@ def writeFile():
 # Write melody
 def createMelody():
     notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
-    melody = "piano: "
+
+    # Set starting note
+    note = randint(0, len(notes)-1)
+    octave = randint(0, 8)
+
+    # Set instrument to piano, set octave
+    melody = "piano: o" + str(octave) + " "
     
     # Create 4 measures of 8 notes
     for x in range(4):
         for y in range(8):
-            melody += notes[randint(0,len(notes)-1)] + " "
+            print_octave = False
+            step = 3
+
+            # Add or subtract 3 from note
+            note += randint(-step,step)
+
+            # Wrap note around if above or below
+            if note > 6:
+                if octave < 8:
+                    octave += 1
+                    print_octave = True
+                note -= 7
+            elif note < 0:
+                if octave > 0:
+                    octave -= 1
+                    print_octave = True
+                note += 7
+
+            # If octave changed, print it
+            if print_octave:
+                melody += "o" + str(octave) + " "
+
+            melody += notes[note] + " "
         melody += "\n"
+
     return melody
 
 # Play alda file
 def playFile():
     subprocess.call('alda play --file "song.txt"')
 
+# Export as midi
+def exportFile():
+    subprocess.call('alda export -f "song.txt" -o "song.mid"')
+
+# Write and play file
 writeFile()
-playFile()
-
-
-# Play alda line
-# subprocess.call('alda play -c "piano: a b c d e f"')
+#playFile()
+exportFile()
